6076d22b8381845e4da8d6e3a40a640aede7ba19
Kasun Jayasanka <officialblackknightcoders@gmail.com>
2023-08-11 18:04:56 +0530

    Work place added to shallow merege

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      name: {firstName:'Kasun',lastName:'Rajapaksha'}
+      name: {firstName:'Kasun',lastName:'Rajapaksha'},
+      company:'XYZ Company'
     }
   }
   render() {
@@ -16,8 +17,12 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
               <h1>{this.state.name.firstName} {this.state.name.lastName}</h1>
+              <p>I worked at {this.state.company}</p>              
               <button onClick={()=>{
-                this.setState({name:{firstName:'Nimal',lastName:'Jayasanka'}})
+                this.setState({
+                  name:{firstName:'Nimal',lastName:'Jayasanka'},
+                  company:'ABC Company'
+              })
                 console.log(this.state)
               }}>
               Change Name </button>
