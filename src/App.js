bab674ddbbf0a9f46708cec13244ed36bf2e75dd
Kasun Jayasanka <officialblackknightcoders@gmail.com>
2023-08-13 16:13:38 +0530

    optimized search method

--- src/App.js
+++ src/App.js
@@ -34,11 +34,23 @@ class App extends Component {
     //console.log('componentDidMount');
   }
 
+  onSearchChange = (event)=>{
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(
+      ()=>{
+        return {searchField};
+      }
+    ); 
+  }
+
   render() {
     console.log('render');
 
-    const filteredMonsters = this.state.monsters.filter((monster)=>{
-      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    const {monsters,searchField} = this.state;
+    const {onSearchChange} = this;
+
+    const filteredMonsters = monsters.filter((monster)=>{
+      return monster.name.toLocaleLowerCase().includes(searchField);
     });
     
 
@@ -50,14 +62,7 @@ class App extends Component {
        type='search' 
        placeholder='search monstors' 
        onChange={
-        (event)=>{
-          const searchField = event.target.value.toLocaleLowerCase();
-          this.setState(
-            ()=>{
-              return {searchField};
-            }
-          ); 
-        }
+        onSearchChange
        } 
        />
        <h1>
