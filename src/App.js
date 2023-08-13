7791842002884b29e37970daa006f4cefe2671b4
Kasun Jayasanka <officialblackknightcoders@gmail.com>
2023-08-13 15:58:53 +0530

    Storing Original Data

--- src/App.js
+++ src/App.js
@@ -9,25 +9,62 @@ class App extends Component {
     this.state = {
       monsters:[
      
-      ]
+      ],
+      searchField:''
     };
-    console.log('constructor');
+
+   //console.log('constructor');
   }
 
   componentDidMount(){
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response=>response.json())
-    .then(users=>this.setState({monsters:users}));
-    console.log('componentDidMount');
+    .then((users)=>
+    this.setState(
+    ()=>{
+      return {monsters:users};
+    },
+    ()=>{
+      console.log(this.state)
+    }
+    )
+    );
+
+
+    //console.log('componentDidMount');
   }
 
   render() {
     console.log('render');
-    return (
+
+    const filteredMonsters = this.state.monsters.filter((monster)=>{
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
+    
+
+    return(
       <div className="App">
-       <h1>{this.state.monsters.map((monster)=>{
+
+       <input 
+       className='search-monsters' 
+       type='search' 
+       placeholder='search monstors' 
+       onChange={
+        (event)=>{
+          const searchField = event.target.value.toLocaleLowerCase();
+          this.setState(
+            ()=>{
+              return {searchField};
+            }
+          ); 
+        }
+       } 
+       />
+       <h1>
+       {filteredMonsters.map((monster)=>{
           return <div key={monster.id}><h1>{monster.name}</h1></div>
-       })}</h1>
+       })}
+       </h1>
       </div>
     );
   }
